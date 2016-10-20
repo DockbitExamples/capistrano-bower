8f56a9019fcf640378b0d51fa7e8ee1c05f2fd24
Tair Assimov <tair@dockbit.com>
2016-10-20 12:41:00 +0200

    Cache npm and bower deps with Hacher

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,16 @@ desc 'Build assets with Bower'
 task :build do
   run_locally do
     within "#{File.expand_path File.dirname(__FILE__)}/../app" do
+      # Get dependencies with Hacher
+      execute :hacher, 'get -k node_app -f package.json'
+      execute :hacher, 'get -k bower_app -f bower.json'
+
       execute :npm, 'install'
       execute :npm, 'run-script build'
+
+      # Cache dependencies with Hacher
+      execute :hacher, 'set -k node_app -f package.json ./node_modules'
+      execute :hacher, 'set -k bower_app -f bower.json ./bower_components'
     end
   end
 end
